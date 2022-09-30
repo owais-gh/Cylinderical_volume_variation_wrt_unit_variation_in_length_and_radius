0a9c00f2b64b07b20295ca1d16f9f3cb93b37d4d
Muhammed <owais.loni4@gmail.com>
2022-09-30 21:34:14 +0300

    Initial commit

--- Circumference-length.py
+++ Circumference-length.py
@@ -1,3 +1,8 @@
+"""
+Variation of volume wrt to circumference and length for the negative values and positive values
+Although in real life it would be impossible to have negative values, it is interesting to observe the variation nonetheless
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 import math
@@ -5,38 +10,38 @@ import math
 hmax,cmax,hmin,cmin,rc,vc= 0,0,0,0,0,0
 H,C,va,ra,voe,roe,vot,rot,vd,ro=[],[],[],[],[],[],[],[],[],[]
 
-print("\nVolume variation wrt length and radius for integers.")
+print("\nVolume variation wrt length and circumference for integers.")
 h=float(input("\nEnter the starting value for length: " ))
-ci=float(input("Enter the starting value for radius: "))
+ci=float(input("Enter the starting value for circumference: "))
 hf=float(input("Enter the final value for length: "))
-cf=float(input("Enter the final value for radius: "))
+cf=float(input("Enter the final value for circumference: "))
 print("\nPlease wait while the values are calculated and plotted ...")
 
 c=ci
 while h<hf:
     while c<cf:
-        v=(((2*h*c)+h-(c*c))*math.pi)
+        v=(((2*h*c)+h-(c*c))/(4*math.pi))
         r=(h/c)
         if v<=0:
-            vol = ((2 * h * c) + h - (c * c))*math.pi
+            vol = (((2 * h * c) + h - (c * c))/(4*math.pi))
             vd.append(v)
             ro.append(r)
             if vol==min(vd):
                 hmin=h
                 cmin=c
         if v>0 and r>0 and r<0.8:
-            vol=((2*h*c)+h-(c*c))*math.pi
+            vol=(((2*h*c)+h-(c*c))/(4*math.pi))
             voe.append(vol)
             roe.append(r)
         if v>0 and r>=0.8 and r<=2:
-            vol=((2*h*c)+h-(c*c))*math.pi
+            vol=(((2*h*c)+h-(c*c))/(4*math.pi))
             va.append(vol)
             ra.append(r)
             if vol==max(va):
                 hmax=h
                 cmax=c
         if v>0 and r>2:
-            vol=((2*h*c)+h-(c*c))*math.pi
+            vol=(((2*h*c)+h-(c*c))/(4*math.pi))
             vot.append(vol)
             rot.append(r)
         H.append(h)
@@ -46,29 +51,29 @@ while h<hf:
     h+=0.05
 
 
-print("\nFor the ratio where V(rad+1)<V(h+1)")
+print("\nFor the ratio where V(c+1)<V(h+1)")
 print("x values:",len(ro))
 print("y values:",len(vd))
 
-print("\nFor the ratio where V(h+1)>V(rad+1)")
+print("\nFor the ratio where V(c+1)>V(h+1)")
 print("0<r<0.8: x values: "+str(len(roe))+"; y values: "+str(len(voe)))
 print("0.8<r<2: x values: "+str(len(ra))+"; y values: "+str(len(va)))
 print("r>2    : x values: "+str(len(rot))+"; y values: "+str(len(vot)))
 
-print("Total values: "+str(len(ro)+len(roe)+len(ra)+len(rot)))
+print("\nTotal values: "+str(len(ro)+len(ra)+len(roe)+len(rot)))
 
-print("\nFor maximum volume increase for V(rad+1)>V(h+1)[i.e., where increasing the radius is better]: \nRatio: "
-      +str(ra[va.index(max(va))])+";\nLength: "+str(hmax)+";\nRadius: "+str(cmax)+";\nVolume increase: "+str(max(va)))
+print("\nFor maximum volume increase for V(c+1)>V(h+1)[i.e., where increasing the circumference is better]: \nRatio: "
+      +str(ra[va.index(max(va))])+";\nLength: "+str(hmax)+";\nCircumference: "+str(cmax)+";\nVolume increase: "+str(max(va)))
 
-print("\nFor maximum volume increase for V(rad+1)<V(h+1)[i.e., where increasing the length is better]: \nRatio: "
-      +str((ro[vd.index(min(vd))]))+";\nLength: "+str(abs(hmin))+";\nRadius: "+str(abs(cmin))+";\nVolume increase: "+str(abs(min(vd))))
+print("\nFor maximum volume increase for V(c+1)<V(h+1)[i.e., where increasing the length is better]: \nRatio: "
+      +str((ro[vd.index(min(vd))]))+";\nLength: "+str(hmin)+";\nCircumference: "+str(cmin)+";\nVolume increase: "+str(min(vd)))
 
 fig, ax = plt.subplots(figsize=(8,6))
 ax.scatter(ro, vd, color='red', marker='x', s=0.1, label='Vd<0')
 ax.scatter(ra,va, color='green', marker='x', s=0.1, label='Vd>0: 0.8<=r<=2')
-ax.scatter(roe,voe, color='blue', marker='x', s=0.1, label='Vd>0: 0<r<0.8')
+ax.scatter(roe,voe, color='blue', marker='x', s=0.1,  label='Vd>0: 0<r<0.8')
 ax.scatter(rot,vot, color='blue', marker='x', s=0.1, label='Vd>0: r>2')
 ax.legend(loc='best', frameon=True, fancybox=False)
-ax.set_title('Vd=V(r+1)-V(h+1) wrt ratio, r(r/c)');
-plt.grid(True)
+ax.set_title('Vd=V(c+1)-V(h+1) wrt ratio, r(h/c)');
+ax.grid(True)
 plt.show()
--- Radius-length.py
+++ Radius-length.py
@@ -1,3 +1,8 @@
+"""
+Variation of volume difference when radius and length are considered
+Although in real life it would be impossible to have negative values, it is interesting to observe the variation nonetheless
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 import math
@@ -5,38 +10,38 @@ import math
 hmax,cmax,hmin,cmin,rc,vc= 0,0,0,0,0,0
 H,C,va,ra,voe,roe,vot,rot,vd,ro=[],[],[],[],[],[],[],[],[],[]
 
-print("\nVolume variation wrt length and circumference for integers.")
+print("\nVolume variation wrt length and radius for integers.")
 h=float(input("\nEnter the starting value for length: " ))
-ci=float(input("Enter the starting value for circumference: "))
+ci=float(input("Enter the starting value for radius: "))
 hf=float(input("Enter the final value for length: "))
-cf=float(input("Enter the final value for circumference: "))
+cf=float(input("Enter the final value for radius: "))
 print("\nPlease wait while the values are calculated and plotted ...")
 
 c=ci
 while h<hf:
     while c<cf:
-        v=(((2*h*c)+h-(c*c))/(4*math.pi))
+        v=(((2*h*c)+h-(c*c))*math.pi)
         r=(h/c)
         if v<=0:
-            vol = (((2 * h * c) + h - (c * c))/(4*math.pi))
+            vol = ((2 * h * c) + h - (c * c))*math.pi
             vd.append(v)
             ro.append(r)
             if vol==min(vd):
                 hmin=h
                 cmin=c
         if v>0 and r>0 and r<0.8:
-            vol=(((2*h*c)+h-(c*c))/(4*math.pi))
+            vol=((2*h*c)+h-(c*c))*math.pi
             voe.append(vol)
             roe.append(r)
         if v>0 and r>=0.8 and r<=2:
-            vol=(((2*h*c)+h-(c*c))/(4*math.pi))
+            vol=((2*h*c)+h-(c*c))*math.pi
             va.append(vol)
             ra.append(r)
             if vol==max(va):
                 hmax=h
                 cmax=c
         if v>0 and r>2:
-            vol=(((2*h*c)+h-(c*c))/(4*math.pi))
+            vol=((2*h*c)+h-(c*c))*math.pi
             vot.append(vol)
             rot.append(r)
         H.append(h)
@@ -46,29 +51,29 @@ while h<hf:
     h+=0.05
 
 
-print("\nFor the ratio where V(c+1)<V(h+1)")
+print("\nFor the ratio where V(rad+1)<V(h+1)")
 print("x values:",len(ro))
 print("y values:",len(vd))
 
-print("\nFor the ratio where V(c+1)>V(h+1)")
+print("\nFor the ratio where V(h+1)>V(rad+1)")
 print("0<r<0.8: x values: "+str(len(roe))+"; y values: "+str(len(voe)))
 print("0.8<r<2: x values: "+str(len(ra))+"; y values: "+str(len(va)))
 print("r>2    : x values: "+str(len(rot))+"; y values: "+str(len(vot)))
 
-print("\nTotal values: "+str(len(ro)+len(ra)+len(roe)+len(rot)))
+print("Total values: "+str(len(ro)+len(roe)+len(ra)+len(rot)))
 
-print("\nFor maximum volume increase for V(c+1)>V(h+1)[i.e., where increasing the circumference is better]: \nRatio: "
-      +str(ra[va.index(max(va))])+";\nLength: "+str(hmax)+";\nCircumference: "+str(cmax)+";\nVolume increase: "+str(max(va)))
+print("\nFor maximum volume increase for V(rad+1)>V(h+1)[i.e., where increasing the radius is better]: \nRatio: "
+      +str(ra[va.index(max(va))])+";\nLength: "+str(hmax)+";\nRadius: "+str(cmax)+";\nVolume increase: "+str(max(va)))
 
-print("\nFor maximum volume increase for V(c+1)<V(h+1)[i.e., where increasing the length is better]: \nRatio: "
-      +str((ro[vd.index(min(vd))]))+";\nLength: "+str(hmin)+";\nCircumference: "+str(cmin)+";\nVolume increase: "+str(min(vd)))
+print("\nFor maximum volume increase for V(rad+1)<V(h+1)[i.e., where increasing the length is better]: \nRatio: "
+      +str((ro[vd.index(min(vd))]))+";\nLength: "+str(abs(hmin))+";\nRadius: "+str(abs(cmin))+";\nVolume increase: "+str(abs(min(vd))))
 
 fig, ax = plt.subplots(figsize=(8,6))
 ax.scatter(ro, vd, color='red', marker='x', s=0.1, label='Vd<0')
 ax.scatter(ra,va, color='green', marker='x', s=0.1, label='Vd>0: 0.8<=r<=2')
-ax.scatter(roe,voe, color='blue', marker='x', s=0.1,  label='Vd>0: 0<r<0.8')
+ax.scatter(roe,voe, color='blue', marker='x', s=0.1, label='Vd>0: 0<r<0.8')
 ax.scatter(rot,vot, color='blue', marker='x', s=0.1, label='Vd>0: r>2')
 ax.legend(loc='best', frameon=True, fancybox=False)
-ax.set_title('Vd=V(c+1)-V(h+1) wrt ratio, r(h/c)');
-ax.grid(True)
+ax.set_title('Vd=V(r+1)-V(h+1) wrt ratio, r(r/c)');
+plt.grid(True)
 plt.show()
